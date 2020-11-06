0beeb838c3aaff5af4ef1f582777e8deb7fdce8e
Alex Gonzalez <71677050+Adgonzalez2018@users.noreply.github.com>
2020-11-06 13:23:01 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("Physics-Lab-Data.csv", sep=",")
+# v1 and r1 are for current = 1.04A r1 is squared
+# v1 and r2 are for current = 2.03A r2 is squared
+data = data[["v1", "r1"]]
+
+
+# going to use r^2 vs v graph
+x, y = data["v1"], data["r1"]
+# get max and min to find range for I1
+min_y_value = np.min(y)
+max_y_value = np.max(y)
+
+range_1 = ((max_y_value) - (min_y_value)) / 2
+
+m, b = np.polyfit(x, y, 1)
+
+print("Range/Uncertainty for Current I = 1.04 A: ", range_1)
+print("Slope: ", m)
+
+# putting r^2 and voltage into graph
+style.use("grayscale")
+pyplot.suptitle('Radius^2 vs. Voltage', fontsize=20)
+pyplot.scatter(x, y, color="navy")
+
+# error bar graphs the uncertainty of the r^2
+pyplot.errorbar(x, y, yerr=range_1, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
+# plotting slope
+pyplot.plot(x, m*x+b, color="green")
+# labels
+pyplot.xlabel('Voltage (v)')
+pyplot.ylabel('R^2 (cm^2)')
+pyplot.show()
+
+
